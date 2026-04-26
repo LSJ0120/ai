85bd8245016c8223daf068ad36c6b0ba508abc8e
lsj <2374409678@qq.com>
2026-04-26 09:11:09 +0800

    pdf

--- src/main/java/com/example/demo/qwen/controller/PdfController.java
+++ src/main/java/com/example/demo/qwen/controller/PdfController.java
@@ -1,14 +1,16 @@
 package com.example.demo.qwen.controller;
 
 
-import com.itheima.ai.repository.ChatHistoryRepository;
-import com.itheima.ai.repository.FileRepository;
+import com.example.demo.qwen.entity.vo.Result;
+import com.example.demo.qwen.repository.ChatHistoryRepository;
+import com.example.demo.qwen.repository.FileRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.reader.ExtractedTextFormatter;
+
 import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
 import org.springframework.ai.reader.pdf.config.PdfDocumentReaderConfig;
 import org.springframework.ai.vectorstore.VectorStore;
